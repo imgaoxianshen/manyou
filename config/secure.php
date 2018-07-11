bea34d9e597c83511e4354ab2c2771f4d847ddaf
imgaoxianshen <690220607@qq.com>
2018-07-11 17:08:01 +0800

    asd

--- config/secure.php
+++ config/secure.php
@@ -3,6 +3,6 @@
 return [
     'token_salt'=>'imgaoxianshen',
     "code_prex"=>'code_',
-    //'pay_back_url'=>'https://www.imgaoxianshen.cn/manyou/public/api/v1/pay/notify'
-    'pay_back_url'=>'https://www.imgaoxianshen.cn/manyou/public/test.php'
+    'pay_back_url'=>'https://www.imgaoxianshen.cn/manyou/public/api/v1/pay/notify'
+    // 'pay_back_url'=>'https://www.imgaoxianshen.cn/manyou/public/test.php'
 ];
