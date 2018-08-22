3c53c3ba59708e99ef792f4730cb0d7565412c4e
imgaoxianshen <690220607@qq.com>
2018-08-22 23:44:03 +0800

    asd

--- application/api/controller/v1/Pay.php
+++ application/api/controller/v1/Pay.php
@@ -7,7 +7,7 @@ use app\api\service\Pay as PayService;
 use app\api\service\WxNotify as WxNotifySercice;
 use app\lib\exception\SuccessMessage;
 use app\api\model\Order;
-use app\api\service\TokenService;
+use app\api\service\Token as TokenService;
 
 class  Pay extends BaseController{
     protected $beforeActionList = [
