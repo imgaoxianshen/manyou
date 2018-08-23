3eb999bbe5b7e873eff9e51c54e97637a8d80831
imgaoxianshen <690220607@qq.com>
2018-08-23 21:28:37 +0800

    asd

--- application/api/controller/v1/Pay.php
+++ application/api/controller/v1/Pay.php
@@ -8,6 +8,8 @@ use app\api\service\WxNotify as WxNotifySercice;
 use app\lib\exception\SuccessMessage;
 use app\api\model\Order;
 use app\api\service\Token as TokenService;
+use app\api\service\Sms;
+use app\lib\enum\SmsTemplate;
 
 class  Pay extends BaseController{
     protected $beforeActionList = [
@@ -23,6 +25,7 @@ class  Pay extends BaseController{
         }else{
             $uid = TokenService::getCurrentUid();
             Order::payOrder($id,$uid);
+            $res = Sms::sendSms($order->get_phone,$order->name,SmsTemplate::START_SEND);
             return new SuccessMessage();
         }
         
